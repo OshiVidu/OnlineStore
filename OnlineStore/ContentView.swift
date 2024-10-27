7190fd07dcfa431fdf213e0d337ead0876a70615
OshiVidu <vidurangioshi@gmail.com>
2024-10-27 15:41:33 +0530

    Create files

--- OnlineStore/ContentView.swift
+++ OnlineStore/ContentView.swift
@@ -8,17 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            TabView(selection: $selectedTab) {
+                HomeView()
+                    .tag(1)
+                    .tabItem {
+                        if (selectedTab == 1){
+                            Image("home")
+                                
+                                
+                        }
+                            else{
+                            Image("homei")
+                                    
+                                   
+                        }
+                    }
+                ExpolreView()
+                
+                    .tag(2)
+                    .tabItem {
+                        if (selectedTab == 2){
+                            Image("search")
+                                
+                                
+                        }
+                            else{
+                            Image("searchi")
+                                    
+                                   
+                        }
+                        
+                    }
+                ExpolreView()
+                
+                    .tag(3)
+                    .tabItem {
+                        if (selectedTab == 3){
+                            Image("cart")
+                                
+                                
+                        }
+                            else{
+                            Image("cart")
+                                    
+                                   
+                        }
+                        
+                    }
+                WalletView()
+                
+                    .tag(4)
+                    .tabItem {
+                        if (selectedTab == 4){
+                            Image("user")
+                                
+                                
+                        }
+                            else{
+                            Image("useri")
+                                    
+                                   
+                        }
+                        
+                    }
+                    
+                
+                
+            }
+            
         }
-        .padding()
+        
+        
+        
+        
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+       
+            ContentView()
+                
+    }
 }
